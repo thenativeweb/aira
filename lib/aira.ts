b4cb7d2fe3ffa79e6b1df0375763f22dc10e7afe
Golo Roden <golo.roden@thenativeweb.io>
2021-06-25 06:39:28 +0200

    feat: Add pattern mode.

--- lib/aira.ts
+++ lib/aira.ts
@@ -1,4 +1,5 @@
 import { getNoteLengths } from './utils/getNoteLengths';
+import { Instrument } from './drivers/Instrument';
 import { MidiValue } from './types/MidiValue';
 import { System8 } from './drivers/System8';
 import { Tb3 } from './drivers/Tb3';
@@ -6,6 +7,7 @@ import { Tr8 } from './drivers/Tr8';
 
 export {
   getNoteLengths,
+  Instrument,
   MidiValue,
   System8,
   Tb3,
--- lib/bin/app.ts
+++ lib/bin/app.ts
@@ -1,6 +1,6 @@
-import { getNoteLengths, MidiValue, System8, Tb3, Tr8 } from '../aira';
-
-const noteLengths = getNoteLengths({ bpm: 137 });
+import { EventEmitter } from 'events';
+import { Instrument, MidiValue, System8, Tb3, Tr8 } from '../aira';
+import * as errors from '../errors';
 
 const tr8 = new Tr8({
   port: 'MX-1 USB1',
@@ -39,65 +39,257 @@ process.on('SIGINT', (): void => {
   process.exit();
 });
 
-let cutoffAndResonance: MidiValue = 0;
+interface DrumStep {
+  type: 'beat' | 'none';
+  velocity?: MidiValue;
+}
 
-setInterval((): void => {
-  tr8.bassDrum({ length: noteLengths.sixteenth });
-}, noteLengths.quarter);
+interface NoteStep {
+  type: 'note' | 'none';
+  velocity?: MidiValue;
+}
 
-setInterval((): void => {
-  tr8.snareDrum({ length: noteLengths.sixteenth });
-}, noteLengths.half);
+type Step = DrumStep | NoteStep;
 
-setTimeout((): void => {
-  setInterval((): void => {
-    tr8.closedHihat({ length: noteLengths.sixteenth });
-  }, noteLengths.quarter);
-}, noteLengths.eigth);
+type Steps8 = [
+  Step, Step, Step, Step,
+  Step, Step, Step, Step
+];
 
-setInterval((): void => {
-  setTimeout((): void => {
-    tb3.playNote({ note: 'a', octave: 1, length: noteLengths.sixteenth, velocity: 127 });
+type Steps16 = [
+  Step, Step, Step, Step,
+  Step, Step, Step, Step,
+  Step, Step, Step, Step,
+  Step, Step, Step, Step,
+];
 
-    cutoffAndResonance = (cutoffAndResonance + 1) % 128 as MidiValue;
-    tb3.setCutoff({ value: cutoffAndResonance });
-    tb3.setResonance({ value: cutoffAndResonance });
+type Steps = Steps16 | Steps8;
 
-    setTimeout((): void => {
-      tb3.playNote({ note: 'a', octave: 2, length: noteLengths.sixteenth, velocity: 127 });
+class Pattern<TSteps extends Steps> {
+  public readonly steps: TSteps;
 
-      cutoffAndResonance = (cutoffAndResonance + 1) % 128 as MidiValue;
-      tb3.setCutoff({ value: cutoffAndResonance });
-      tb3.setResonance({ value: cutoffAndResonance });
+  public constructor (steps: TSteps) {
+    this.steps = steps;
+  }
+}
 
+/* eslint-disable id-length, @typescript-eslint/naming-convention */
+const X: Step = { type: 'beat', velocity: 127 as MidiValue };
+const x: Step = { type: 'beat', velocity: 80 as MidiValue };
+const _: Step = { type: 'none' };
+
+const crescendo = function <TStep> (steps: TStep[]): TStep[] {
+  const increasePerStep = Math.floor(127 / steps.length);
+
+  return steps.map((step, index): TStep => ({
+    ...step,
+    velocity: increasePerStep * (index + 1)
+  }));
+};
+
+const bd = {
+  a: new Pattern<Steps16>([ X, _, _, _, X, _, _, _, X, _, _, _, X, _, _, _ ]),
+  b: new Pattern<Steps16>([ X, _, _, _, X, _, _, _, X, _, _, _, X, _, x, _ ]),
+  c: new Pattern<Steps16>(crescendo([ x, x, x, x, x, x, x, x, X, X, X, X, X, X, X, X ]) as any)
+};
+
+const ch = {
+  a: new Pattern<Steps16>([ _, _, X, _, _, _, X, _, _, _, X, _, _, _, X, _ ]),
+  b: new Pattern<Steps16>([ _, _, X, _, _, _, X, _, _, x, X, x, _, x, X, _ ])
+};
+/* eslint-enable id-length, @typescript-eslint/naming-convention */
+
+interface Track {
+  name: string;
+  instrument: Instrument;
+  volume: MidiValue;
+  mute: boolean;
+  solo: boolean;
+}
+
+class Metronome extends EventEmitter {
+  public readonly msPer4th: number;
+
+  private readonly msPer32nd: number;
+
+  private intervalId?: NodeJS.Timeout;
+
+  public constructor ({ bpm }: {
+    bpm: number;
+  }) {
+    super();
+
+    this.msPer4th = 60_000 / bpm;
+    this.msPer32nd = this.msPer4th / 8;
+  }
+
+  public start (): void {
+    if (this.intervalId) {
+      return;
+    }
+
+    let bar = 0;
+    let counter32nd = 0;
+
+    this.intervalId = setInterval((): void => {
+      if (counter32nd % 8 === 0) {
+        this.emit('click', {
+          bar,
+          step4th: counter32nd / 8,
+          step8th: counter32nd / 4,
+          step16th: counter32nd / 2,
+          step32nd: counter32nd
+        });
+      } else if (counter32nd % 4 === 0) {
+        this.emit('click', {
+          bar,
+          step4th: undefined,
+          step8th: counter32nd / 4,
+          step16th: counter32nd / 2,
+          step32nd: counter32nd
+        });
+      } else if (counter32nd % 2 === 0) {
+        this.emit('click', {
+          bar,
+          step4th: undefined,
+          step8th: undefined,
+          step16th: counter32nd / 2,
+          step32nd: counter32nd
+        });
+      } else {
+        this.emit('click', {
+          bar,
+          step4th: undefined,
+          step8th: undefined,
+          step16th: undefined,
+          step32nd: counter32nd
+        });
+      }
+
+      counter32nd += 1;
+
+      if (counter32nd > 31) {
+        counter32nd = 0;
+        bar += 1;
+      }
+    }, this.msPer32nd);
+  }
+
+  public stop (): void {
+    if (!this.intervalId) {
+      return;
+    }
+
+    clearInterval(this.intervalId);
+    this.intervalId = undefined;
+  }
+}
+
+class Song {
+  private readonly tracks: Track[];
+
+  private readonly patterns: (Pattern<Steps>[] | undefined)[];
+
+  private readonly metronome: Metronome;
+
+  public constructor ({ bpm, tracks, patterns }: {
+    bpm: number;
+    tracks: Track[];
+    patterns: (Pattern<Steps>[] | undefined)[];
+  }) {
+    this.tracks = tracks;
+    this.patterns = patterns;
+    this.metronome = new Metronome({ bpm });
+  }
+
+  public async play (): Promise<void> {
+    tr8.stop();
+    tb3.stop();
+    system8.stop();
+
+    await new Promise<void>((resolve): void => {
       setTimeout((): void => {
-        tb3.playNote({ note: 'a', octave: 1, length: noteLengths.sixteenth, velocity: 127 });
+        resolve();
+      }, 100);
+    });
 
-        cutoffAndResonance = (cutoffAndResonance + 1) % 128 as MidiValue;
-        tb3.setCutoff({ value: cutoffAndResonance });
-        tb3.setResonance({ value: cutoffAndResonance });
-      }, noteLengths.sixteenth);
-    }, noteLengths.sixteenth);
-  }, noteLengths.sixteenth);
-}, noteLengths.quarter);
+    this.metronome.on('click', ({ bar, step4th, step8th, step16th, step32nd }: {
+      bar: number;
+      step4th: number | undefined;
+      step8th: number | undefined;
+      step16th: number | undefined;
+      step32nd: number;
+    }): void => {
+      if (step16th === undefined) {
+        return;
+      }
 
-setInterval((): void => {
-  system8.playNote({ note: 'a', octave: 3, length: noteLengths.quarter * 3, velocity: 127 });
-  system8.playNote({ note: 'e', octave: 4, length: noteLengths.quarter * 3, velocity: 127 });
-  system8.playNote({ note: 'c', octave: 5, length: noteLengths.quarter * 3, velocity: 127 });
-  setTimeout((): void => {
-    system8.playNote({ note: 'f', octave: 3, length: noteLengths.quarter * 3, velocity: 127 });
-    system8.playNote({ note: 'c', octave: 4, length: noteLengths.quarter * 3, velocity: 127 });
-    system8.playNote({ note: 'a', octave: 4, length: noteLengths.quarter * 3, velocity: 127 });
-    setTimeout((): void => {
-      system8.playNote({ note: 'e', octave: 3, length: noteLengths.quarter * 3, velocity: 127 });
-      system8.playNote({ note: 'b', octave: 3, length: noteLengths.quarter * 3, velocity: 127 });
-      system8.playNote({ note: 'g', octave: 4, length: noteLengths.quarter * 3, velocity: 127 });
-      setTimeout((): void => {
-        system8.playNote({ note: 'g', octave: 3, length: noteLengths.quarter * 3, velocity: 127 });
-        system8.playNote({ note: 'd', octave: 4, length: noteLengths.quarter * 3, velocity: 127 });
-        system8.playNote({ note: 'b', octave: 4, length: noteLengths.quarter * 3, velocity: 127 });
-      }, noteLengths.whole);
-    }, noteLengths.whole);
-  }, noteLengths.whole);
-}, noteLengths.whole * 4);
+      const currentPatterns = this.patterns[bar];
+
+      if (!currentPatterns) {
+        return this.metronome.stop();
+      }
+
+      for (const [ trackIndex, pattern ] of currentPatterns.entries()) {
+        const step = pattern.steps[step16th];
+
+        if (step.type === 'none') {
+          continue;
+        } else if (step.type === 'beat') {
+          if (trackIndex === 0) {
+            (this.tracks[trackIndex].instrument as Tr8).bassDrum({
+              velocity: step.velocity,
+              length: (this.metronome.msPer4th / 4) * 0.9
+            });
+          } else {
+            (this.tracks[trackIndex].instrument as Tr8).closedHihat({
+              velocity: step.velocity,
+              length: (this.metronome.msPer4th / 4) * 0.9
+            });
+          }
+        } else {
+          throw new errors.OperationInvalid('Invalid step type.');
+        }
+      }
+    });
+
+    this.metronome.start();
+  }
+}
+
+const song = new Song({
+  bpm: 137,
+
+  tracks: [
+    {
+      name: 'Bassdrum',
+      instrument: tr8,
+      volume: 100,
+      mute: false,
+      solo: false
+    },
+    {
+      name: 'Closed Hi-Hat',
+      instrument: tr8,
+      volume: 100,
+      mute: false,
+      solo: false
+    }
+  ],
+
+  patterns: [
+    // Introduction
+    [ bd.a, ch.a ],
+    [ bd.b, ch.b ],
+
+    // Bridge
+    [ bd.a, ch.a ],
+    [ bd.c, ch.b ]
+  ]
+});
+
+/* eslint-disable @typescript-eslint/no-floating-promises */
+(async (): Promise<void> => {
+  await song.play();
+})();
+/* eslint-enable @typescript-eslint/no-floating-promises */
